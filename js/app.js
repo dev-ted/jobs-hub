596167f676126a1878e21f06cbc069589f760303
dev-ted <devted19@gmail.com>
2021-08-07 18:34:14 -0400

    replaced added coded data with data from api

--- js/app.js
+++ js/app.js
@@ -9,14 +9,13 @@ const cta_login = document.querySelector(
 const cta_register = document.querySelector(
   ".header__container__cta__register__button"
 );
+const jobsCount = document.querySelector(".jobs__results__count");
+
+let jobsCon = document.querySelector(".jobs__container");
+let dataHtml = " ";
+let dataCount = " ";
 
 let isOPen = false;
-
-
-
-
-
-
 
 
 menu_icon.addEventListener("click", () => {
@@ -48,16 +47,8 @@ window.onscroll = () => {
 //get jobs
 
 function getJobs() {
-       fetch(
-         "https://job-search4.p.rapidapi.com/simplyhired/search?query=b&page=200",
-         {
-           method: "GET",
-           headers: {
-             "x-rapidapi-key":
-               "5d394e054dmshdec26f7d82df997p1f3d91jsn559ab944eacf",
-             "x-rapidapi-host": "job-search4.p.rapidapi.com",
-           },
-         }
+       return fetch(
+         "../data/data.json"
        )
          .then((res) => res.json())
          .then((jobs) => {
@@ -71,12 +62,45 @@ function getJobs() {
 //show jobs
 
 function showJobs(jobs) {
-       console.log(jobs);
+        console.log(jobs);
+        dataCount += `<span>Showing  ${jobs.length} jobs</span>`;
+        jobsCount.innerHTML = dataCount;
+
+        jobs.forEach(job => {
+                console.log("jobs>>>>", job.company);
+                
+                dataHtml += `  <div class="jobs__container__card">
+                                <div class="jobs__container__card__top">
+                                        <img src="${job.logo}"
+                                                alt="" class="jobs__container__card__img">
+                                        <i class="fa fa-ellipsis-v"></i>
+                                </div>
+                                <div class="jobs__container__card__role">
+                                        <span>${job.roleName}</span>
+                                </div>
+                                <div class="jobs__container__card__description">
+                                        <p>
+                                                ${job.requirements.content}
+
+
+                                        </p>
+                                </div>
+                                <div class="jobs__container__card__cta">
+                                        <a href="${job.applicationLink}" target = "_blank" class="jobs__container__card__cta__link">Apply Now</a>
+                                </div>
+                       
+
+                </div>`;
+        })
+
+        jobsCon.innerHTML = dataHtml;
+        
+        
 }
 
 
 
 //call funcs
- getJobs().then((jobs) => {
-   showJobs(jobs);
- });
+getJobs().then(jobsData => {
+        showJobs(jobsData);
+});
